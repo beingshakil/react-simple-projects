e1c59e4a49fc66519f69703da5664cd359d21346
Beingshakil <shakil15-14472@diu.edu.bd>
2024-12-06 21:22:05 +0600

    Todo list update

--- Todo-list/src/Components/Todo.jsx
+++ Todo-list/src/Components/Todo.jsx
@@ -1,41 +1,93 @@
-import { useRef } from 'react'
-import todo_icon from '../assets/todo_icon.png'
-import TodoItems from './TodoItems'
+import { useEffect, useRef, useState } from "react";
+import todo_icon from "../assets/todo_icon.png";
+import TodoItems from "./TodoItems";
 
 const Todo = () => {
+  const [todoList, setTodoList] = useState([]);
 
-    const inputRef = useRef();
+  const inputRef = useRef();
 
-    // eslint-disable-next-line no-unused-vars
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
 
-    const add = () => {
-        const inputText = inputRef.current.value.trim();
-        console.log(inputText);
+    if (inputText === "") {
+      return null;
     }
+
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
+    };
+
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+  };
+
+  const deleteTodo = (id) => {
+    setTodoList((prevTodo) => {
+        return prevTodo.filter((todo) => todo.id !== id)
+    })
+  }
+
+  const toggle = (id) => {
+    setTodoList((prevTodo) => {
+        return prevTodo.map((todo)=> {
+            if(todo.id === id){
+                return {...todo, isComplete: !todo.isComplete}
+            }
+            return todo;
+        })
+    })
+  }
+
+  useEffect(()=> {
+    console.log(todoList);
+  }, [todoList])
 
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
-       
-        {/* --- Title --- */}
-        <div className="flex items-center mt-7 gap-2">
-            <img className='w-8' src={todo_icon} alt="" />
-            <h1 className="text-3xl font-semibold">To-Do List</h1>
-        </div>
+      {/* --- Title --- */}
+      <div className="flex items-center mt-7 gap-2">
+        <img className="w-8" src={todo_icon} alt="" />
+        <h1 className="text-3xl font-semibold">To-Do List</h1>
+      </div>
 
-        {/* ---  Input Box  --- */}
-        <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-            <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type='text' placeholder='Add your task'/>
-            <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>Add +</button>
-        </div>
+      {/* ---  Input Box  --- */}
+      <div className="flex items-center my-7 bg-gray-200 rounded-full">
+        <input
+          ref={inputRef}
+          className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
+          type="text"
+          placeholder="Add your task"
+        />
+        <button
+          onClick={add}
+          className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+        >
+          Add +
+        </button>
+      </div>
 
-        {/* ---  Todo List  --- */}
-        <div>
-            <TodoItems text="Learn Coding"/>
-            <TodoItems text="Watch Movies"/>
-        </div>
+      {/* ---  Todo List  --- */}
 
+      <div>
+        {todoList.map((item, index) => {
+          return (
+            <TodoItems key={index}  text={item.text}  id={item.id}
+              isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle}/>
+          );
+        })}
+
+        {/*  
+        
+          <TodoItems text="Learn Coding" />
+          <TodoItems text="Watch Movies" />       
+     */}
+
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
